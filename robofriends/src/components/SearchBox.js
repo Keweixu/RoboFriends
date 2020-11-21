3ebb33a3ec8e4427c6aa04ff88b867f7b8266ed4
Kewei Xu <Kewei.xu@vanderbilt.edu>
2020-11-20 20:15:00 -0600

    Initial

---
+++ robofriends/src/components/SearchBox.js
@@ -0,0 +1,33 @@
+// import React from 'react';
+
+// const SearchBox = ({searchChanged }) => {
+//     return (
+//         <div className='pa2'>
+//             <input
+//                 className='pa3 ba b--green bg-lightest-blue'
+//                 type='search'
+//                 placeholder='search robots'
+//                 onChange={searchChanged}
+//             />
+//         </div>
+//     )
+// }
+
+// export default SearchBox;
+
+import React from 'react';
+
+const SearchBox = ({searchChanged }) => {
+  return (
+    <div className='pa2'>
+      <input
+        className='pa3 ba b--green bg-lightest-blue'
+        type='search'
+        placeholder='search robots'
+        onChange={searchChanged}
+      />
+    </div>
+  );
+}
+
+export default SearchBox;
